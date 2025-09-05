3327a64d5dcdc592b2db4f6b5736d9ead26bb7b7
VuxlifyDev <vuxnpc@gmail.com>
2025-09-05 18:05:21 -0500

    Update game.lua

--- arena/game.lua
+++ arena/game.lua
@@ -1,1 +1,4 @@
-
+local plr = game:GetService("Players").LocalPlayer
+shared.LoreTags = shared.LoreTags or {}
+shared.LoreTags[plr] = "<font color='rgb(255,85,85)'>[Bloodbound]</font> "
+plr:SetAttribute("CanSummon", false)
