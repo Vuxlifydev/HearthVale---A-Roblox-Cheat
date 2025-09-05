6be11a6f4bf6a69d5590f07ea66c922c79f50a0f
VuxlifyDev <vuxnpc@gmail.com>
2025-09-05 18:05:05 -0500

    Update game.lua

--- forest/game.lua
+++ forest/game.lua
@@ -1,1 +1,4 @@
-
+local plr = game:GetService("Players").LocalPlayer
+shared.LoreTags = shared.LoreTags or {}
+shared.LoreTags[plr] = "<font color='rgb(0,255,127)'>[ForestWarden]</font> "
+plr:SetAttribute("CanSummon", true)
