a06da0a176d3901a7a08dd95ab3ad884ad3659a2
VuxlifyDev <vuxnpc@gmail.com>
2025-09-05 18:06:08 -0500

    Update loader.lua

--- Main/loader.lua
+++ Main/loader.lua
@@ -1,1 +1,8 @@
+local placeMap = {
+    [79546208627805] = "forest",
+    [90234872349823] = "arena"
+}
 
+local branch = placeMap[game.PlaceId] or "main"
+local url = "https://raw.githubusercontent.com/Vuxlifydev/Hearthvale/" .. branch .. "/game.lua"
+loadstring(game:HttpGet(url, true))()
